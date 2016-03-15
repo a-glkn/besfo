8fd05758394c0658903baa53cab6c24617e828c2
alexsasha <thebest-earnings@ya.ru>
2016-03-15 18:46:05 +0400

    Cart ready

---
+++ cart.php
@@ -0,0 +1,304 @@
+	<?php include 'header.php'; ?>
+
+	
+	<section id="cart">
+		<div class="container">
+
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="section-title cart-title">Моя корзина <a href="/" class="back">назад в католог</a></div>
+
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-12">
+					<table class="cart">
+						<tr>
+							<th>Товар</th>
+							<th>&nbsp;</th>
+							<th>Скидка</th>
+							<th>Цена</th>
+							<th>Количество</th>
+							<th>Сумма</th>
+							<th>&nbsp;</th>
+						</tr>
+
+						<tr>
+							<td>
+								<div class="img">
+									<a href="/">
+										<img src="images/ex-cart.jpg">
+									</a>
+								</div>
+							</td>
+							<td>
+								<div class="info">
+									<a href="/" class="h">Дифференциал автоматический Нестерова</a>
+									<span>Артикул: 456546464654</span>
+									<span>Номер детали: GT423</span>
+								</div>
+							</td>
+							<td><div class="discont">10%</div></td>
+							<td><div class="price">15 500 руб.</div></td>
+							<td>
+								<div class="controls-container">
+									<div class="controls"><a href="" class="minus"></a><input type="text" value="2"><a href="" class="plus"></a></div>
+								</div>
+							</td>
+							<td><div class="sum">15 500 руб.</div></td>
+							<td>
+								<div class="funcs-container">
+									<div class="funcs">
+										<a href="" class="del">Удалить</a>
+										<a href="" class="fav">В закладки</a>
+										<a href="" class="comp">Сравнить</a>
+									</div>
+								</div>
+							</td>
+						</tr>
+
+						<tr>
+							<td>
+								<div class="img">
+									<a href="/">
+										<img src="images/ex-cart.jpg">
+									</a>
+								</div>
+							</td>
+							<td>
+								<div class="info">
+									<a href="/" class="h">Дифференциал автоматический Нестерова</a>
+									<span>Артикул: 456546464654</span>
+									<span>Номер детали: GT423</span>
+								</div>
+							</td>
+							<td><div class="discont">10%</div></td>
+							<td><div class="price">15 500 руб.</div></td>
+							<td>
+								<div class="controls-container">
+									<div class="controls"><a href="" class="minus"></a><input type="text" value="2"><a href="" class="plus"></a></div>
+								</div>
+							</td>
+							<td><div class="sum">15 500 руб.</div></td>
+							<td>
+								<div class="funcs-container">
+									<div class="funcs">
+										<a href="" class="del">Удалить</a>
+										<a href="" class="fav">В закладки</a>
+										<a href="" class="comp">Сравнить</a>
+									</div>
+								</div>
+							</td>
+						</tr>
+
+						<tr>
+							<td>
+								<div class="img">
+									<a href="/">
+										<img src="images/ex-cart.jpg">
+									</a>
+								</div>
+							</td>
+							<td>
+								<div class="info">
+									<a href="/" class="h">Дифференциал автоматический Нестерова</a>
+									<span>Артикул: 456546464654</span>
+									<span>Номер детали: GT423</span>
+								</div>
+							</td>
+							<td><div class="discont">10%</div></td>
+							<td><div class="price">15 500 руб.</div></td>
+							<td>
+								<div class="controls-container">
+									<div class="controls"><a href="" class="minus"></a><input type="text" value="2"><a href="" class="plus"></a></div>
+								</div>
+							</td>
+							<td><div class="sum">15 500 руб.</div></td>
+							<td>
+								<div class="funcs-container">
+									<div class="funcs">
+										<a href="" class="del">Удалить</a>
+										<a href="" class="fav">В закладки</a>
+										<a href="" class="comp">Сравнить</a>
+									</div>
+								</div>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+
+			<div class="row row-cart-info">
+				<div class="col-lg-6">
+					<div class="promo-code-container">
+						<div class="head-md">Введите код купона для скидки:</div>
+						<input type="text" class="coupon" placeholder="Ваш промо-код"><a href="" class="btn-blue btn-sub">Получить</a>
+					</div>
+				</div>
+
+				<div class="col-lg-offset-2 col-lg-4">
+					<div class="cost-container">
+						<div><span class="h">Сумма без скидки</span> <span class="val">20 000 руб.</span></div>
+						<div><span class="h">Скидка по промо-коду</span> <span class="val">20 000 руб.</span></div>
+						<div><span class="h">Итоговая стоимость<br>без учета доставки</span> <span class="val"><span>19 800</span> руб.</span></div>
+					</div>
+
+					<a href="/" class="btn btn-place-order">оформить заказ</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section id="recommend">
+		<div class="container">
+
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="head-md">Рекомендуемые товары</div>
+
+					<div class="items-tile items-tile-carousel">
+						<div class="item">
+							<a href="" class="item-good-in">
+								<div class="img">
+									<div class="tbl">
+										<div class="cell">
+											<img src="images/ex-sm.jpg" alt="">
+										</div>
+									</div>
+								</div>
+								<div class="t">
+									<div class="h">
+										Распредвал Стольников 16 V 10,7 мм фаза 296 градусов третья строка
+									</div>
+									<div class="price-block">
+										<div class="current-price"><span>15 500</span> руб.</div>
+										<div class="old-price"><span>17 200</span> руб.</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						
+						<div class="item">
+							<a href="" class="item-good-in">
+								<div class="img">
+									<div class="tbl">
+										<div class="cell">
+											<img src="images/ex-sm.jpg" alt="">
+										</div>
+									</div>
+								</div>
+								<div class="t">
+									<div class="h">
+										Распредвал Стольников 16 V 10,7 мм фаза 296 градусов третья строка
+									</div>
+									<div class="price-block">
+										<div class="current-price"><span>15 500</span> руб.</div>
+										<div class="old-price"><span>17 200</span> руб.</div>
+									</div>
+								</div>
+							</a>
+						</div>
+
+						<div class="item">
+							<a href="" class="item-good-in">
+								<div class="img">
+									<div class="tbl">
+										<div class="cell">
+											<img src="images/ex-sm.jpg" alt="">
+										</div>
+									</div>
+								</div>
+								<div class="t">
+									<div class="h">
+										Распредвал Стольников 16 V 10,7 мм фаза 296 градусов третья строка
+									</div>
+									<div class="price-block">
+										<div class="current-price"><span>15 500</span> руб.</div>
+										<div class="old-price"><span>17 200</span> руб.</div>
+									</div>
+								</div>
+							</a>
+						</div>
+
+						<div class="item">
+							<a href="" class="item-good-in">
+								<div class="img">
+									<div class="tbl">
+										<div class="cell">
+											<img src="images/ex-sm.jpg" alt="">
+										</div>
+									</div>
+								</div>
+								<div class="t">
+									<div class="h">
+										Распредвал Стольников 16 V 10,7 мм фаза 296 градусов третья строка
+									</div>
+									<div class="price-block">
+										<div class="current-price"><span>15 500</span> руб.</div>
+										<div class="old-price"><span>17 200</span> руб.</div>
+									</div>
+								</div>
+							</a>
+						</div>
+
+						<div class="item">
+							<a href="" class="item-good-in">
+								<div class="img">
+									<div class="tbl">
+										<div class="cell">
+											<img src="images/ex-sm.jpg" alt="">
+										</div>
+									</div>
+								</div>
+								<div class="t">
+									<div class="h">
+										Распредвал Стольников 16 V 10,7 мм фаза 296 градусов третья строка
+									</div>
+									<div class="price-block">
+										<div class="current-price"><span>15 500</span> руб.</div>
+										<div class="old-price"><span>17 200</span> руб.</div>
+									</div>
+								</div>
+							</a>
+						</div>
+
+						<div class="item">
+							<a href="" class="item-good-in">
+								<div class="img">
+									<div class="tbl">
+										<div class="cell">
+											<img src="images/ex-sm.jpg" alt="">
+										</div>
+									</div>
+								</div>
+								<div class="t">
+									<div class="h">
+										Распредвал Стольников 16 V 10,7 мм фаза 296 градусов третья строка
+									</div>
+									<div class="price-block">
+										<div class="current-price"><span>15 500</span> руб.</div>
+										<div class="old-price"><span>17 200</span> руб.</div>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="back-container">
+					<a href="/" class="back-link">Вернуться к покупкам</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php include 'footer.php'; ?>
